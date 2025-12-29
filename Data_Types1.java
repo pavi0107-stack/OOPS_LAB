c0e8e3e0b3a69dcd283ea48fdb7ff81a82e3676d
pavi0107-stack <pavisiva2709@gmail.com>
2025-12-29 11:55:28 +0530

    Add files via upload

---
+++ Data_Types1.java
@@ -0,0 +1,20 @@
+class Data_Types1
+{
+public static void main(String[] args)
+{
+byte age=50;
+byte exp=20;
+short daily_salary=30000;
+int monthly_salary=daily_salary*30;
+long funds=67676767676767L;
+float increase=5.29f;
+double acc_increase=5.29752d;
+System.out.println("Employee age:"+age);
+System.out.println("Experience:"+exp);
+System.out.println("Salary per day:"+daily_salary);
+System.out.println("Salary per month:"+monthly_salary);
+System.out.println("Company's funds:"+funds);
+System.out.println("Increase percentage of salary after every year:"+increase);
+System.out.println("Accurate increase percentage of salary:"+acc_increase);
+}
+}
---
+++ Data_Types2.java
@@ -0,0 +1,10 @@
+class Data_Types2
+{
+public static void main(String[] ffgggh)
+{
+char choice='Y';
+boolean is_choice_yes=true;
+System.out.println("Choice:"+choice);
+System.out.println("Is choice Yes?:"+is_choice_yes);
+}
+}
